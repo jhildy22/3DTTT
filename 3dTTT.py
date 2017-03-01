b2e691c3cb7803324862cc2a80e59b47ad1c3368
jhildy22 <jhighflyer02@gmail.com>
2017-03-01 15:08:22 -0500

    First running version

--- 3dTTT.py
+++ 3dTTT.py
@@ -1,37 +1,12 @@
 __author__ = 'Richthofen'
 import numpy as np
 
-'''
-newGame:
-	displayMenu
-	reset
-	move
-
-displayMenu:
-	print menu
-
-
-move:
-	checkMove
-
-checkMove:
-	checkWinner/move
-
-checkWinner:
-	endGame/move
-
-endGame:
-	drawBoard
-	playAgain
-
-playAgain:
-	reset/exit
-'''
 
 
 # Global var to hold board
 board = np.array([[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]], np.int32)
 turn = 'X'
+turn_key = {'X':1, 'O':4}
 
 # prints menu, prompts user to continue: go -> move, exit -> exit_game
 def newGame():
@@ -62,49 +37,125 @@ def displayMenu():
 
 # reset matrix user turn
 def reset():
+    global board
+    global turn
     board = np.array([[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]], np.int32)
+    turn = 'X'
 
 
 # process user move input and send to helper to verify
 def move(error):
+
+    level_row_dictionary = {'r':0, 'y':1, 'g':2, 'a':0, 'b':1, 'c':2, 1:0, 2:1, 3:2 }
     print error
     user_move = raw_input("Player " + turn + ":")
-    level = user_move.split()[0][0]
-    col = user_move.split()[1][1]
-    row = user_move.split()[1][0]
+
+    if len(user_move.split()) != 2:
+        move("Invalid input!")
 
     try:
-        if int(col) in [1,2,3] and str(row).lower in ['a','b','c'] and str(level).lower() in ['r', 'y','g']:
-            checkMove(str(level), str(col), int(row))
+        level = str(user_move.split()[0][0]).lower()
+        row = str(user_move.split()[1][0]).lower()
+        col = int(user_move.split()[1][1])
+
+        if level in ['r', 'y','g'] and row in ['a','b','c'] and col in [1,2,3]:
+            checkMove(level_row_dictionary[level], level_row_dictionary[row], level_row_dictionary[col])
+
     except:
-        move("Invalid input: Please try again")
+        move("Invalid Input!")
 
 
 
-def checkMove(level, col, row):
+def checkMove(level, row, col):
+    global turn
+
     if board[level, col, row] == 0:
-        board[level, col, row] = turn
+        board[level, col, row] = turn_key[turn]
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
         checkWinner()
     else:
-        endGame(turn)
+        endGame("forgot")
 
 
 def checkWinner():
-    print "hi there"
+    loop = [0,1,2]
+    count = 0
+
+    for lev in loop:
+        count = 0
+
+        for row in loop:
+            count = 0
+            for col in loop:
+                count += board[lev,col,row]
+                if count == 3 or count == 12:
+                    endGame()
+        for col in loop:
+            count = 0
+            for row in loop:
+                count += board[lev,col,row]
+                if count == 3 or count == 12:
+                    endGame("")
 
 
-def endGame(turn):
+    for col in loop:
+        count = 0
+
+        for lev in loop:
+            count = 0
+            for row in loop:
+                count += board[lev,col,row]
+                if count == 3 or count == 12:
+                    endGame()
+        for row in loop:
+            count = 0
+            for lev in loop:
+                count += board[lev,col,row]
+                if count == 3 or count == 12:
+                    endGame("")
+
+
+    for row in loop:
+        count = 0
+
+        for lev in loop:
+            count = 0
+            for col in loop:
+                count += board[lev,col,row]
+                if count == 3 or count == 12:
+                    endGame("")
+        for col in loop:
+            count = 0
+            for lev in loop:
+                count += board[lev,col,row]
+                if count == 3 or count == 12:
+                    endGame("")
+    move("")
+
+
+
+
+def endGame(error):
+
+    if error == "forgot":
+        print "Player " + turn + ", You forgot the board! You loose!"
+        playAgain = raw_input("Do you want to play again? y/n ")
+        if playAgain.lower() == 'y':
+            newGame()
+        else:
+            exit_game()
+
+
     if turn == 'X':
         winner = 'O'
     else:
         winner = 'X'
 
-    print "We have a winner! Congratulations " + winner + "you have won this round."
-    playAgain = raw_input("Do you want to play again? y/n")
+    print "We have a winner! Congratulations " + winner + " you have won this round."
+    playAgain = raw_input("Do you want to play again? y/n ")
     if playAgain.lower() == 'y':
         newGame()
     else:
@@ -120,7 +171,7 @@ def exit_game():
     if user_exit == "y" or user_exit == "Y":
         exit()
     else:
-        reset()
+        newGame()
 
 
 
@@ -133,3 +184,4 @@ main()
 
 
 # hi there how are you
+# today
